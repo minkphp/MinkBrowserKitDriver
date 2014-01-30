eb1da1962026a4ecb42907394d69f4ca61a0bda4
thewilkybarkid <chriswilkinson84@gmail.com>
2014-01-30 08:49:56 +0000

    Fake the SCRIPT_FILENAME server variable to allow the base URL to be correctly guessed

--- tests/Behat/Mink/Driver/BrowserKitDriverTest.php
+++ tests/Behat/Mink/Driver/BrowserKitDriverTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Behat\Mink\Driver;
 
 use Behat\Mink\Driver\BrowserKitDriver;
+use Behat\Mink\Session;
 use Symfony\Component\HttpKernel\Client;
 
 /**
@@ -22,5 +23,16 @@ class BrowserKitDriverTest extends HeadlessDriverTest
     protected function pathTo($path)
     {
         return 'http://localhost'.$path;
+    }
+
+    public function testBaseUrl()
+    {
+        $client = new Client(require(__DIR__.'/../../../app.php'));
+        $driver = new BrowserKitDriver($client, 'http://localhost/foo/');
+        $session = new Session($driver);
+
+        $session->visit('http://localhost/foo/index.php');
+        $this->assertEquals(200, $session->getStatusCode());
+        $this->assertEquals('http://localhost/foo/index.php', $session->getCurrentUrl());
     }
 }

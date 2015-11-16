2c043056da3c1eceea3903be098ce716d739664b
Benjamin Lévêque <benjamin@leveque.me>
2015-11-16 19:15:04 +0100

    inverse the condition

--- src/BrowserKitDriver.php
+++ src/BrowserKitDriver.php
@@ -809,11 +809,12 @@ class BrowserKitDriver extends CoreDriver
     {
         $node = null;
 
-        if ($crawler instanceof \IteratorAggregate) {
-            $node = $crawler->getNode(0);
-        } else {
+        if ($crawler instanceof \Iterator) {
+            // for symfony 2.3 compatibility as getNode is not public before symfony 2.4
             $crawler->rewind();
             $node = $crawler->current();
+        } else {
+            $node = $crawler->getNode(0);
         }
 
         if (null !== $node) {

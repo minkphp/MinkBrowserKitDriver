0d9fb7cd801aef697097da27043d1a59887529db
Julien Menth <j.menth@crowdfox.com>
2018-04-05 12:21:34 +0200

    Pass serverParameters to submit function of the symfony BrowserKit Client
    This: https://github.com/symfony/symfony/pull/26791 needs to be merged

--- src/BrowserKitDriver.php
+++ src/BrowserKitDriver.php
@@ -693,7 +693,7 @@ class BrowserKitDriver extends CoreDriver
             }
         }
 
-        $this->client->submit($form);
+        $this->client->submit($form, array(), $this->serverParameters);
 
         $this->forms = array();
     }
